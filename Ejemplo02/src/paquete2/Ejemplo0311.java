23866901e9c6a23bff0b9472845d68ebaaf780e7
Bananin-DEV <mjvaldivieso6@utpl.edu.ec>
2025-10-30 11:11:43 -0500

    limpieza de buffer

---
+++ Ejemplo02/src/paquete2/Ejemplo0311.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo0311 {
+    public static void main(String[] args) {
+        // Ingreso de datos por teclado
+        // uso de la clase Scanner
+        Scanner entrada = new Scanner(System.in);
+        String nombre;
+        String apellido;
+        int nacimiento;
+        int ciclo;
+        String direccion;       
+        System.out.println("Ingrese el nombre del estudiante");
+        nombre = entrada.nextLine(); // nextLine: obtiene una cadena ingresada
+                                    // por teclado
+        
+        System.out.println("Ingrese el apellido del estudiante");
+        apellido = entrada.nextLine();
+        
+        System.out.println("Ingrese el año de nacimiento del estudiante");
+        nacimiento = entrada.nextInt();
+        
+        System.out.println("Ingrese su cilo academico en numero");
+        ciclo = entrada.nextInt();
+        entrada.nextLine();
+        
+        System.out.println("Ingresar su direccion");
+        direccion = entrada.nextLine();
+        
+        System.out.printf("Datos del estudiante\nNombre: %s\nApellido:"
+                + " %s\nAño de Nacimiento: %d\nCiclo academico: %d\nDireccion: %s\n", 
+                nombre,
+                apellido,
+                nacimiento,
+                ciclo,
+                direccion);
+        
+    }
+}
