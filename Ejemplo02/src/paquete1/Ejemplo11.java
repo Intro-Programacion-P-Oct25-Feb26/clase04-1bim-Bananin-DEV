7e8b89effb6a5c23ab2b21a025411fef0920b0fa
Bananin-DEV <mjvaldivieso6@utpl.edu.ec>
2025-10-30 10:43:25 -0500

    datos estudiante

--- Ejemplo02/src/paquete1/Ejemplo11.java
+++ Ejemplo02/src/paquete1/Ejemplo11.java
@@ -12,36 +12,31 @@ import java.util.Scanner;
  *
  * @author reroes
  */
-public class Ejemplo1 {
+public class Ejemplo11 {
 
     public static void main(String[] args) {
         // Ingreso de valores por teclado
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US); // para ingresar los decimales con 
-                                        // punto
-                                        // no con coma
+        // punto
+        // no con coma
         // declarar las variables
         String nombreAsignatura1;
         String nombreAsignatura2;
         double notaAsignatura1;
         double notaAsignatura2;
-        
+
         System.out.println("Ingrese nombre de asignatura 1");
         nombreAsignatura1 = entrada.nextLine(); // nextLine me permite recibir una cadena con scanner
         System.out.println("Ingrese nota de asignatura 1");
         notaAsignatura1 = entrada.nextDouble();
-        System.out.println("Ingrese nombre de asignatura 2");
-        nombreAsignatura2 = entrada.nextLine();
-        System.out.println("Ingrese nota de asignatura 2");
-        notaAsignatura2 = entrada.nextDouble();
-        
-        System.out.printf("Asignatura 1: %s\nNota 1:%.2f\nAsignatura 2:%s\n"
-                + "Nota 2: %.2f\n",
+
+        // %.2f significa que voy a recibir un decimal y lo presenta de forma redondeada y legible
+        System.out.printf("Asignatura 1: %s\nNota 1:%.2f\n",
                 nombreAsignatura1,
-                notaAsignatura1,
-                nombreAsignatura2,
-                notaAsignatura2);
-   
+                notaAsignatura1
+        );
+
     }
-    
+
 }
